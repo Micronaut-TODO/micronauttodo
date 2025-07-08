9daceb47b94ef9a4d6706a36acbd363b5f746c66
Sergio del Amo <sergio.delamo@softamo.com>
2025-07-08 13:52:00 +0200

    use persistence test

--- src/test/java/com/micronauttodo/security/dbauth/SignInControllerTest.java
+++ src/test/java/com/micronauttodo/security/dbauth/SignInControllerTest.java
@@ -1,15 +1,17 @@
 package com.micronauttodo.security.dbauth;
 
 import com.micronauttodo.BrowserRequest;
-import com.micronauttodo.MicronautTestNoPersistence;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@MicronautTestNoPersistence
+@Testcontainers(disabledWithoutDocker = true)
+@MicronautTest(transactional = false)
 class SignInControllerTest {
 
     @Test
